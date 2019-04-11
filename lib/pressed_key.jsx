22061f8a1971c1e8f0f8d45ff9236857f42a5b7a
Dante Cantal <danteacantal@gmail.com>
2019-04-11 18:35:15 -0400

    Trying different color schemes

--- lib/pressed_key.jsx
+++ lib/pressed_key.jsx
@@ -12,5 +12,22 @@ class PressedKey {
         } else if (this.val == "t") {
             this.pos = [];
         }
+        this.squareWidth = 60;
+        this.squareHeight = 60;
+    }
+
+    draw(ctx) {
+        ctx.strokeStyle = ""
+        ctx.beginPath();
+        ctx.moveTo(this.pos[0], this.pos[1]);
+        ctx.lineTo(this.pos[0] + squareWidth, this.pos[1]);
+        ctx.quadraticCurveTo(this.pos[0] + squareWidth + 10, this.pos[1], this.pos[0] + squareWidth + 10, this.pos[1] + 10);
+        ctx.lineTo(this.pos[0] + squareWidth + 10, this.pos[1] + squareHeight);
+        ctx.quadraticCurveTo(this.pos[0] + squareWidth + 10, this.pos[1] + squareHeight + 10, this.pos[0] + squareWidth, this.pos[1] + squareHeight + 10);
+        ctx.lineTo(this.pos[0], this.pos[1] + squareHeight + 10);
+        ctx.quadraticCurveTo(this.pos[0] - 10, this.pos[1] + squareHeight + 10, this.pos[0] - 10, this.pos[1] + squareHeight);
+        ctx.lineTo(this.pos[0] - 10, this.pos[1] + 10);
+        ctx.quadraticCurveTo(this.pos[0] - 10, this.pos[1], this.pos[0], this.pos[1]);
+
     }
 }

63b467b09a339e0cb015a3ea8084e5ba2bdaa68f
Dante Cantal <danteacantal@gmail.com>
2019-04-11 18:21:26 -0400

    Added proof of concept to index.html and started making actual js files for the different components

---
+++ lib/pressed_key.jsx
@@ -0,0 +1,16 @@
+class PressedKey {
+    constructor(props) {
+        this.val = props.val;
+        if (this.val == "q") {
+            this.pos = [];
+        } else if (this.val == "w") {
+            this.pos = [];
+        } else if (this.val == "e") {
+            this.pos = [];
+        } else if (this.val == "r") {
+            this.pos = [];
+        } else if (this.val == "t") {
+            this.pos = [];
+        }
+    }
+}
